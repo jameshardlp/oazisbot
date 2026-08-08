194bb37dfbb9e81f1a6d91e4e233369eddb68bcc
jameshardlp <jameshardlp@gmail.com>
2026-08-08 16:57:16 +0300

    Clean up telegram module initialization
    Removed imports and __init__ list from telegram/__init__.py.

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,17 +1,1 @@
-from . import client
-from . import media
-from . import posting
-from . import quota
-from . import rate_limit
-from . import scheduler
-from . import sender
-
-__init__ = [
-    "client",
-    "media",
-    "posting",
-    "quota",
-    "rate_limit",
-    "scheduler",
-    "sender",
-]
+"""Telegram-часть бота."""
