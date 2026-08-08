e06612bacbf88bf64b1cb0aa45074cdf8a0e6ece
jameshardlp <jameshardlp@gmail.com>
2026-08-08 16:56:58 +0300

    Clean up payments module imports
    Removed import statements and __init__ list from payments module.

--- payments/__init__.py
+++ payments/__init__.py
@@ -1,11 +1,1 @@
-from . import aurapay
-from . import freekassa
-from . import orders
-from . import webhooks
-
-__init__ = [
-    "aurapay",
-    "freekassa",
-    "orders",
-    "webhooks",
-]
+"""Модуль платёжных систем."""
