807cf8ec51bc85bf63e88263f9a6b5c97099b5cf
jameshardlp <jameshardlp@gmail.com>
2026-08-08 17:15:27 +0300

    Fix import statement for config module

--- storage.py
+++ storage.py
@@ -9,7 +9,7 @@ import logging
 from datetime import datetime
 from typing import List, Dict, Any
 
-from .config import USERS_FILE, HISTORY_FILE, SCHEDULE_FILE, USAGE_FILE
+from config import USERS_FILE, HISTORY_FILE, SCHEDULE_FILE, USAGE_FILE
 
 logger = logging.getLogger(__name__)
 
