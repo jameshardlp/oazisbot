2d8720c9dd9694461f479d2701f87f6d2654044d
jameshardlp <jameshardlp@gmail.com>
2026-08-22 19:05:01 +0300

    Update __init__.py

--- bot_modules/handlers/__init__.py
+++ bot_modules/handlers/__init__.py
@@ -1,9 +1,5 @@
-"""Регистрация всех хендлеров команд и callback'ов."""
-from . import admin
-from . import basic
-from . import broadcast
-from . import moderation
-from . import payment
-from .basic import *
-from .broadcast import broadcast_command, broadcast_callback, get_broadcast_conversation_handler, handle_broadcast_content
+"""Инициализация обработчиков команд."""
+from .basic import start_command, help_command
+from .broadcast import broadcast_command, broadcast_callback, get_broadcast_conversation_handler
 from .resend import get_resend_conversation_handler
+from .admin import register_admin_handlers, start, stats, schedule_command
