59d392a95a78bad2807278b6b4d8ceb6c469bd21
jameshardlp <jameshardlp@gmail.com>
2026-08-22 18:27:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import os
 import sys
 
 from aiohttp import web
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
 
 logging.basicConfig(
     level=logging.INFO,
