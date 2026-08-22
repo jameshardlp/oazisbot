d64f51ddf6fa20b66d0933fc24dc5d190e4ebb6b
jameshardlp <jameshardlp@gmail.com>
2026-08-22 20:26:16 +0300

    Add photo handler registration to init

--- bot_modules/handlers/__init__.py
+++ bot_modules/handlers/__init__.py
@@ -3,3 +3,4 @@ from .basic import register_basic_handlers
 from .broadcast import broadcast_command, broadcast_callback, get_broadcast_conversation_handler
 from .resend import get_resend_conversation_handler
 from .admin import register_admin_handlers
+from .photo import register_photo_handler
