b39fdc931bdd4ba8a7292daf1d026ecbb1f17fc2
jameshardlp <jameshardlp@gmail.com>
2026-08-22 19:59:01 +0300

    Update __init__.py

--- bot_modules/handlers/__init__.py
+++ bot_modules/handlers/__init__.py
@@ -1,5 +1,5 @@
 """Инициализация обработчиков команд."""
-from .basic import start_command, help_command
+from .basic import register_basic_handlers
 from .broadcast import broadcast_command, broadcast_callback, get_broadcast_conversation_handler
 from .resend import get_resend_conversation_handler
-from .admin import register_admin_handlers, start, stats, schedule_command
+from .admin import register_admin_handlers
